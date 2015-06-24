b64221024908201429e3565cd73233c4c779b2fc
Devan Kestel <devan.kestel@gmail.com>
2015-06-24 10:01:36 -0400

    Added has_many :production_lines to factory and carmodel models.

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -1,6 +1,6 @@
 class CarModel < ActiveRecord::Base
     belongs_to :manufacturer
-
+    has_many :production_lines
     has_many :factories, :through => :production_lines
     validates :name, presence: true, uniqueness: true
 
--- app/models/factory.rb
+++ app/models/factory.rb
@@ -1,7 +1,7 @@
 class Factory < ActiveRecord::Base
   belongs_to :manufacturer
   has_many :car_models, :through => :production_lines
-
+  has_many :production_lines
   validates :city, uniqueness: true, presence: true
   validates :state, uniqueness: true, presence: true
 
