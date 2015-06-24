e50d7a735f274c3359745d256dc798aa53dc2650
Leslie Brown <lkb@alum.mit.edu>
2015-06-24 10:05:02 -0400

    Added production lines and car models relationship in factory.rb

--- app/models/factory.rb
+++ app/models/factory.rb
@@ -1,7 +1,7 @@
 class Factory < ActiveRecord::Base
   belongs_to :manufacturer
-  has_and_belongs_to_many :car_models
-
+  has_many :car_models
+  has_many :production_lines, through: :car_models
   validates :city, uniqueness: true, presence: true
   validates :state, uniqueness: true, presence: true
 
