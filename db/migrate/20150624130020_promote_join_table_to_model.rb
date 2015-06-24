8274f0dbf7192c2b1cb400871c10127a8ae33798
Leslie Brown <lkb@alum.mit.edu>
2015-06-24 09:48:09 -0400

    Didn't create a model so I don't know if this is right.

--- db/migrate/20150624130020_promote_join_table_to_model.rb
+++ db/migrate/20150624130020_promote_join_table_to_model.rb
@@ -1,4 +1,6 @@
 class PromoteJoinTableToModel < ActiveRecord::Migration
   def change
+    add_column :car_models_factories, :annual_quota, :integer
+    rename_table :car_models_factories, :production_lines
   end
 end
