b589dce114623ce6d4067a3fe52b87012e802aee
Erik Jenks <erikjenks@gmail.com>
2015-07-07 09:20:36 -0400

    added before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login
+
+
+  private
+
+  def require_login
+    user = User.find_by(email: params[:email])
+    unless user && user.authenticate(params[:password])
+      session[:user] = params[:email] # I still don't really understand the session[] magic
+      flash[:error] = "Please Log In To access this"
+      redirect_to root_url
+    end
+  end
 end
