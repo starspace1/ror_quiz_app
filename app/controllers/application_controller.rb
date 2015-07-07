95cd7a9cd6fb758f9c57a2098545c0bfc3e0d785
Erik Jenks <erikjenks@gmail.com>
2015-07-07 10:12:33 -0400

    committing to checkout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   private
   def require_login
-    user = User.find_by(email: params[:email])
-    unless user && user.authenticate(params[:password])
-      session[:user] = params[:email] # I still don't really understand the session[] magic
-      flash[:error] = "Please Log In To access this"
-      redirect_to root_url
+    unless session[:user]
+      redirect_to login_url
+      return false
     end
   end
 
-  def current_user
-    @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
-  end
 end
