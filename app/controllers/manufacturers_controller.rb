95099444f92c9c8998c79c2001073c41d5d0008c
Leslie Brown <lkb@alum.mit.edu>
2015-07-08 09:20:31 -0400

    refactored manufacturers controller
    @manufacturer = Manufacturer.find(params[:id]) only in one place now

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,29 +1,31 @@
 class ManufacturersController < ApplicationController
   before_action :require_login, only: [:edit, :update, :destroy]
+  before_action :get_manufacturer, except: [:index]
 
   def index
     @manufacturers = Manufacturer.all
   end
 
   def show
-    @manufacturer = Manufacturer.find(params[:id])
   end
 
   def edit
-    @manufacturer = Manufacturer.find(params[:id])
   end
 
   def update
-    @manufacturer = Manufacturer.find(params[:id])
     @manufacturer.update(params.require(:manufacturer).permit(:name, :domestic))
     flash[:notice] = "Manufacturer successfully updated!"
     redirect_to manufacturer_url
   end
 
   def destroy
-    @manufacturer = Manufacturer.find(params[:id])
     @manufacturer.destroy
     redirect_to root_url
   end
 
+  private
+
+  def get_manufacturer
+    @manufacturer = Manufacturer.find(params[:id])
+  end
 end
